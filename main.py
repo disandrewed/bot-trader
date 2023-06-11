0e9bc612d759c6066a9cda524d491a4457051ce9
Andrew Tang <disandrewed@gmail.com>
2023-06-11 11:52:01 +1000

    omitted actions from history getter

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import yfinance as yf
 import pandas_ta as ta
 import time as timer
 
-
 # asset class
 class MyAsset:
     def __init__(self, name):
@@ -13,7 +12,8 @@ class MyAsset:
         
 
 # asset list
-asset_list = ["AMZN"]
+asset_list = ["AMZN", "AAPL", "GOOG"]
+
 asset_class_list = []
 for asset_name in asset_list:
     print("creating asset for: " + asset_name)
@@ -23,24 +23,24 @@ for asset_name in asset_list:
 
 while True:
     for asset in asset_class_list:
-        start_date = (datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d')
-        df = asset.getter.history(start=start_date, interval='1m')
+        #start_date = (datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d')
+        df = asset.getter.history(period='1d', interval='1m', actions=False)
 
+        print()
         print(asset.ticker)
         print(df.tail(5))
-        del df['Dividends']
-        del df['Stock Splits']
-        del df['Volume']
-        df['SMA_fast'] = ta.sma(df['Close'], 10)
-        df['SMA_slow'] = ta.sma(df['Close'], 30)
-        
+
+        df['ma_fast'] = ta.sma(df['Close'], 10)
+        df['ma_slow'] = ta.sma(df['Close'], 30)
         
         
         price = df.iloc[-1]['Close']
-        if df.iloc[-1]['SMA_fast'] > df.iloc[-1]['SMA_slow'] and not asset.holding:
-            print("buy " + asset.ticker)
-            df.holding = True
-        elif df.iloc[-1]['SMA_fast'] < df.iloc[-1]['SMA_slow'] and asset.holding:
-            print("sell " + asset.ticker)
-    print("waiting...")
+        
+        if df.iloc[-1]['ma_fast'] > df.iloc[-1]['ma_slow'] and not asset.holding:
+            print("buy " + asset.ticker + "at price: " + price)
+            asset.holding = True
+        elif df.iloc[-1]['ma_fast'] < df.iloc[-1]['ma_slow'] and asset.holding:
+            print("sell " + asset.ticker + "at price: " + price)
+            asset.holding = False
+    print("\nwaiting...")
     timer.sleep(60)
